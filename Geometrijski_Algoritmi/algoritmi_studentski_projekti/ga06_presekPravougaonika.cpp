a72fbd7a3dc41609ef9b4f07b3b1e3518880d702
matfija <mi16099@alas.matf.bg.ac.rs>
2020-11-08 01:17:29 +0100

    Implementiran metod brišuće prave

--- Geometrijski_Algoritmi/algoritmi_studentski_projekti/ga06_presekPravougaonika.cpp
+++ Geometrijski_Algoritmi/algoritmi_studentski_projekti/ga06_presekPravougaonika.cpp
@@ -1,69 +1,208 @@
 #include "ga06_presekPravougaonika.h"
 #include <fstream>
 
+/* Konstrukcija pravougaonika od dve tacke */
+Pravougaonik::Pravougaonik(QPoint &topLeft, QPoint &bottomRight)
+    : QRect(topLeft, bottomRight), xLevo(topLeft.x()), xDesno(bottomRight.x()),
+                                   yGore(topLeft.y()), yDole(bottomRight.y())
+{ }
+
+
+/* Konstrukcija pravougaonika od parametara */
+Pravougaonik::Pravougaonik(int x, int y, int w, int h)
+    : QRect(x, y, w, h), xLevo(x), xDesno(x+w),
+                         yGore(y+h), yDole(y)
+{ }
+
+/* Struktura za poredjenje pravougaonika */
+bool PravougaonikComp::operator()(const Pravougaonik *l, const Pravougaonik *d) const
+{
+    /* Manji je levlji pravougaonik */
+    if (l->xLevo != d->xLevo) {
+        return l->xLevo < d->xLevo;
+    /* Manji je levlji pravougaonik */
+    } else if (l->xDesno != d->xDesno) {
+        return l->xDesno < d->xDesno;
+    /* Inace se porede pokazivaci */
+    } else {
+        return l < d;
+    }
+}
+
+/* Struktura za poredjenje preseka */
+bool PresekComp::operator()(const Presek &l, const Presek &d) const
+{
+    /* Komparator pravougaonika */
+    PravougaonikComp pc;
+
+    /* Jednakost nezavisno od redosleda */
+    if ((l.first == d.first && l.second == d.second) ||
+        (l.first == d.second && l.second == d.first)) {
+        return false;
+    /* Inace poredjenje pravougaonika */
+    } else if (l.first != d.first) {
+        return pc(l.first, d.first);
+    } else {
+        return pc(l.second, d.second);
+    }
+}
+
+/* Konstrukcija dogadjaja */
+Dogadjaj::Dogadjaj(Pravougaonik *p, TipDogadjaja t)
+    : pravougaonik(p), tipDogadjaja(t), y(t == GORNJA ? p->yGore : p->yDole)
+{ }
+
+/* Dohvatanje levog intervala pravougaonika */
+int Dogadjaj::getXLevo() const
+{
+    return pravougaonik->xLevo;
+}
+
+/* Dohvatanje desnog intervala pravougaonika */
+int Dogadjaj::getXDesno() const
+{
+    return pravougaonik->xDesno;
+}
+
+/* Struktura za poredjenje dogadjaja */
+bool DogadjajComp::operator()(const Dogadjaj &l, const Dogadjaj &d) const
+{
+    /* Manji je dogadjaj na visoj poziciji */
+    if (l.y != d.y) {
+        return l.y > d.y;
+        /* Manji je dogadjaj na levljoj poziciji */
+    } else if (l.getXLevo() != d.getXLevo()) {
+        return l.getXLevo() < d.getXLevo();
+        /* Manji je dogadjaj na levljoj poziciji */
+    } else if (l.getXDesno() != d.getXDesno()) {
+        return l.getXDesno() < d.getXDesno();
+        /* Manji je dogadjaj dodavanja od izbacivanja */
+    } else {
+        return l.tipDogadjaja < d.tipDogadjaja;
+    }
+}
+
+/* Konstrukcija algoritma */
 PresekPravougaonika::PresekPravougaonika(QWidget *pCrtanje,
                                          int pauzaKoraka,
                                          std::string imeDatoteke,
                                          int brojPravougaonika)
     :AlgoritamBaza(pCrtanje, pauzaKoraka)
 {
+    /* Inicijalizacija niza pravougaonika */
     if (imeDatoteke == "")
-      generisiNasumicnePravougaonike(brojPravougaonika);
+        generisiNasumicnePravougaonike(brojPravougaonika);
     else
-      ucitajPodatkeIzDatoteke(imeDatoteke);
+        ucitajPodatkeIzDatoteke(imeDatoteke);
 }
 
+/* Deinicijalizacija algoritma */
+PresekPravougaonika::~PresekPravougaonika()
+{
+    for (auto i = 0ul; i < _n; i++) {
+        delete _pravougaonici[i];
+    }
+    delete [] _pravougaonici;
+}
+
+/* Algoritam zasnovan na strategiji podeli pa vladaj */
 void PresekPravougaonika::pokreniAlgoritam()
 {
+    /* Obavestavanje pozivaoca o finalizovanoj animaciji */
     emit animacijaZavrsila();
 }
 
+/* Iscrtavanje tekuceg stanja algoritma */
 void PresekPravougaonika::crtajAlgoritam(QPainter *painter) const
 {
+    /* Odustajanje u slucaju greske */
     if (!painter) return;
 
+    /* Iscrtavanje svakog pravougaonika */
     for (auto i = 0ul; i < _n; i++) {
         painter->drawRect(*_pravougaonici[i]);
     }
 }
 
+/* Algoritam zasnovan na brisucoj (pokretnoj) pravoj */
 void PresekPravougaonika::pokreniNaivniAlgoritam()
 {
-    struct timespec ts = { 2, 0 };
-    nanosleep(&ts, nullptr);
+    /* Popunjavanje niza dogadjaja */
+    for (auto i = 0ul; i < _n; i++) {
+        _dogadjaji.insert(Dogadjaj(_pravougaonici[i], GORNJA));
+        _dogadjaji.insert(Dogadjaj(_pravougaonici[i], DONJA));
+    }
+
+    /* Prolazak redom kroz dogadjaje */
+    for (const auto &dogadjaj : _dogadjaji) {
+        auto pravougaonik = const_cast<Pravougaonik *>(dogadjaj.pravougaonik);
+
+        /* Obrada nailaska na gornju stranicu */
+        if (dogadjaj.tipDogadjaja == GORNJA) {
+            /* Nalazenje svih trenutnih preseka */
+            const auto preseci = _status.nadjiPreseke(pravougaonik);
+            _preseci.insert(std::cbegin(preseci), std::cend(preseci));
+
+            /* Ubacivanje novog pravougaonika u status */
+            _status.insert(pravougaonik);
+        /* Obrada nailaska na donju stranicu */
+        } else {
+            /* Izbacivanje starog pravougaonika iz statusa */
+            _status.erase(pravougaonik);
+        }
+    }
 }
 
+/* Pravljenje slucajnih pravougaonika */
 void PresekPravougaonika::generisiNasumicnePravougaonike(int brojPravougaonika)
 {
+    /* Alokacija potrebnog prostora */
     _n = brojPravougaonika;
-    _pravougaonici = new QRect*[_n];
+    _pravougaonici = new Pravougaonik *[_n];
 
+    /* Generisanje duplo veceg broja tacaka */
     const auto tacke = generisiNasumicneTacke(2*brojPravougaonika);
+
+    /* Za svaki par pokusaj pravljenja pravougaonika */
     for (auto i = 0ul; i < tacke.size(); i+=2) {
         auto tacka1 = tacke[i];
         auto tacka2 = tacke[i+1];
 
+        /* Preskakanje losih parova */
+        if (tacka1.x() == tacka2.x() &&
+            tacka1.y() == tacka2.y()) {
+            continue;
+        }
+
+        /* Prva treba da bude levo */
         if (tacka1.x() > tacka2.x()) {
             std::swap(tacka1.rx(), tacka2.rx());
         }
 
+        /* Prva treba da bude gore */
         if (tacka1.y() < tacka2.y()) {
             std::swap(tacka1.ry(), tacka2.ry());
         }
 
-        _pravougaonici[i/2] = new QRect(tacka1, tacka2);
+        /* Pravljenje pomocu sredjenih tacaka */
+        _pravougaonici[i/2] = new Pravougaonik(tacka1, tacka2);
     }
 }
 
+/* Citanje pravougaonika iz datoteke */
 void PresekPravougaonika::ucitajPodatkeIzDatoteke(std::string imeDatoteke)
 {
+    /* Otvaranje zadatog fajla */
     std::ifstream datoteka(imeDatoteke);
-    datoteka >> _n;
 
-    _pravougaonici = new QRect*[_n];
+    /* Alokacija potrebnog prostora */
+    datoteka >> _n;
+    _pravougaonici = new Pravougaonik *[_n];
+
+    /* Ucitavanje svakog pravougaonika pojedinacno */
     for (auto i = 0ul; i < _n; i++) {
         int x, y, w, h;
         datoteka >> x >> y >> w >> h;
-        _pravougaonici[i] = new QRect(x, y, w, h);
+        _pravougaonici[i] = new Pravougaonik(x, y, w, h);
     }
 }
--- Geometrijski_Algoritmi/algoritmi_studentski_projekti/ga06_presekPravougaonika.h
+++ Geometrijski_Algoritmi/algoritmi_studentski_projekti/ga06_presekPravougaonika.h
@@ -3,22 +3,216 @@
 
 #include "algoritambaza.h"
 
-class PresekPravougaonika : public AlgoritamBaza
-{
+#include <ext/pb_ds/assoc_container.hpp>
+
+/* Struktura koja predstavlja pravougaonik */
+struct Pravougaonik : QRect {
+    /* Konstruktor i destruktor klase */
+    Pravougaonik(QPoint &, QPoint &);
+    Pravougaonik(int, int, int, int);
+    virtual ~Pravougaonik() = default;
+
+    /* Cuvanje intervala za brisucu pravu */
+    const int xLevo, xDesno,
+              yGore, yDole;
+};
+
+/* Struktura za poredjenje pravougaonika */
+struct PravougaonikComp {
+    bool operator()(const Pravougaonik *, const Pravougaonik *) const;
+};
+
+/* Definisanje preseka */
+using Presek = std::pair<Pravougaonik *, Pravougaonik *>;
+
+/* Struktura za poredjenje preseka */
+struct PresekComp {
+    bool operator()(const Presek &, const Presek &) const;
+};
+
+/* Definisanje skupa preseka */
+using IntersecSet = std::set<Presek, PresekComp>;
+
+/* Politika azuriranja cvorova stabla; sabloni
+ * su iteratori, kao i dva koja zanemarujemo:
+ * funkcija za poredjenje i politika alokacije */
+template <class CIterator,
+          class Iterator,
+          class, class>
+struct IntervalUpdatePolicy {
+
+    /* Definisanje metapodatka kao celobrojne
+     * vrednosti, posto intervalno drvo cuva
+     * maksimalnu desnu tacku poddrveta */
+    using metadata_type = int;
+
+    /* Neophodan virtuelni destruktor */
+    virtual ~IntervalUpdatePolicy() = default;
+
+    /* Virtuelne metode iteracije */
+    virtual CIterator node_begin() const = 0;
+    virtual CIterator node_end() const = 0;
+
+
+    /* Provera presecanja dva intervala */
+    bool imaPreseka(const Pravougaonik *i1,
+                    CIterator i2)
+    {
+        return i1->xLevo <= (**i2)->xDesno
+               && (**i2)->xLevo <= i1->xDesno;
+    }
+
+    /* Trazenje intervala u podstablu */
+    void pretrazi(CIterator it,
+                  Pravougaonik *i,
+                  IntersecSet &preseci)
+    {
+        /* Baza indukcije je prazno podstablo */
+        if (it == node_end())
+            return;
+
+        /* Hvatanje preseka sa korenom */
+        if (imaPreseka(i, it)) {
+            /* Izdvajanje pravougaonika */
+            auto* p = **it;
+
+            /* Komparator pravougaonika */
+            PravougaonikComp pc;
+
+            /* Dodavanje u dobrom poretku */
+            if (pc(i, p)) {
+                preseci.emplace(i, p);
+            } else {
+                preseci.emplace(p, i);
+            }
+        }
+
+        /* Uzimanje dostupne dece */
+        const auto levo = it.get_l_child();
+        const auto desno = it.get_r_child();
+
+        /* Indikator obostranog obilaska */
+        auto idiDesno = true;
+
+        /* Provera levog sina ako ima nade */
+        if (levo != node_end() &&
+            levo.get_metadata() >= i->xLevo){
+            const auto velicina = preseci.size();
+            pretrazi(levo, i, preseci);
+
+            /* Odluka za obostrani obilazak */
+            idiDesno = velicina < preseci.size();
+        }
+
+        /* Provera desnog sina ako ima nade */
+        if (idiDesno) {
+            pretrazi(desno, i, preseci);
+        }
+    }
+
+    /* Pronalazak svih preklapajucih intervala,
+     * pocevsi od korena intervalnog stabla */
+    IntersecSet nadjiPreseke(Pravougaonik *i)
+    {
+        IntersecSet preseci;
+        pretrazi(node_begin(), i, preseci);
+        return preseci;
+    }
+
+    /* Popravka stabla nakon izmene, kako
+     * bi se odrzala invarijanta drveta */
+    void operator()(Iterator it,
+                    CIterator end_it)
+    {
+        /* Podrazumevano je to desna
+         * granica tekuceg intervala */
+        auto metadata = (**it)->xDesno;
+
+        /* Azuriranje nalevo */
+        const auto levo = it.get_l_child();
+        if (levo != end_it) {
+            metadata = std::max(
+                metadata,
+                levo.get_metadata());
+        }
+
+        /* Azuriranje nadesno */
+        const auto desno = it.get_r_child();
+        if (desno != end_it) {
+            metadata = std::max(
+                metadata,
+                desno.get_metadata());
+        }
+
+        /* Azuriranje metapodatka, uz neophodno
+         * uveravanje kompilatora const castom */
+        const_cast<int&>(
+            it.get_metadata())
+            = metadata;
+    }
+};
+
+/* Definisanje intervalnog drveta */
+using IntervalTree = __gnu_pbds::tree<Pravougaonik *, /* nas interval */
+                     __gnu_pbds::null_type, /* pomocni tip nas ne zanima */
+                     PravougaonikComp, /* podrazumevano uredjenje */
+                     __gnu_pbds::rb_tree_tag, /* crveno-crno stablo */
+                     IntervalUpdatePolicy>; /* nasa politika */
+
+/* Enumeracija tipa dogadjaja */
+enum TipDogadjaja {GORNJA, DONJA};
+
+/* Struktura koja predstavlja dogadjaj */
+struct Dogadjaj {
+    /* Konstruktor i destruktor klase */
+    Dogadjaj(Pravougaonik *, TipDogadjaja);
+
+    /* Cuvanje pravougaonika i tipa */
+    const Pravougaonik *pravougaonik;
+    const TipDogadjaja tipDogadjaja;
+
+    /* Cuvanje visine dogadjaja */
+    const int y;
+
+    /* Dohvataci koordinata */
+    int getXLevo() const;
+    int getXDesno() const;
+};
+
+/* Struktura za poredjenje dogadjaja */
+struct DogadjajComp {
+    bool operator()(const Dogadjaj &, const Dogadjaj &) const;
+};
+
+/* Definisanje reda dogadjaja */
+using EventQueue = std::set<Dogadjaj, DogadjajComp>;
+
+/* Klasa koja predstavlja algoritam */
+class PresekPravougaonika : public AlgoritamBaza {
 public:
+    /* Konstruktor i destruktor klase */
     PresekPravougaonika(QWidget *, int, std::string = "",
                         int = BROJ_NASUMICNIH_TACAKA);
+    virtual ~PresekPravougaonika();
 
-    void pokreniAlgoritam();
-    void crtajAlgoritam(QPainter *) const;
-    void pokreniNaivniAlgoritam();
+    /* Virtuelni metodi iz natklase */
+    void pokreniAlgoritam() final;
+    void crtajAlgoritam(QPainter *) const final;
+    void pokreniNaivniAlgoritam() final;
 
 private:
+    /* Rad sa podacima, inicijalizacija */
     void generisiNasumicnePravougaonike(int);
     void ucitajPodatkeIzDatoteke(std::string);
 
-    QRect** _pravougaonici;
+    /* Niz pravougaonika, pokazivaca */
+    Pravougaonik **_pravougaonici;
     unsigned int _n;
+
+    /* Strukture za metod brisuce prave */
+    EventQueue _dogadjaji;
+    IntervalTree _status;
+    IntersecSet _preseci;
 };
 
 #endif // GA06_PRESEKPRAVOUGAONIKA_H
