218170c829fb852e011849c09d6149a6c53ede55
danijelasimic <danijela.p9@gmail.com>
2020-12-08 13:39:37 +0100

    DCEL - dodate mnoge pomocne funkcije. Prateci opis iz knjige dopuniti implementaciju.

--- Geometrijski_Algoritmi/algoritmi_sa_vezbi/ga06_dcel.cpp
+++ Geometrijski_Algoritmi/algoritmi_sa_vezbi/ga06_dcel.cpp
@@ -1,6 +1,284 @@
 #include "ga06_dcel.h"
 
-DCEL::DCEL()
-{
 
+#include <iostream>
+#include <fstream>
+
+/***********************************************************************/
+/*                               DCEL                                  */
+/***********************************************************************/
+
+/*
+DCEL::DCEL(std::string imeDatoteke, int h, int w)
+    :_vertices{}, _edges{}, _fields{}
+{
+    std::ifstream in(imeDatoteke);
+    std::string tmp;
+
+    in >> tmp;
+    if (tmp.compare("OFF") != 0)
+    {
+        std::cout << "Wrong file format: " << imeDatoteke << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int vertexNum, edgeNum, fieldNum;
+    in >> vertexNum >> edgeNum >> fieldNum;
+
+    * Kreiranje temena. */
+    /* Za ozbiljnije testranje:
+     * Proveriti dokumentaciju za off format.
+     * Ovde je prilagodjeno za 2d. I za velicinu naseg kanvasa.
+     * Odnosno, podrazumeva se ispravan ulaz.
+
+    float tmpx, tmpy, tmpz;
+    int x, y;
+    Vertex* v;
+    for(int i=0; i<vertexNum; i++)
+    {
+        * Koordinate u off foramatu su realne, i najcesce [-1, 1]
+         * (pp da je uvek tako, da ne ulazimo u detalje)
+         * Ovde ih namestam da slika bude na sredini kanvasa.
+         *
+        in >> tmpx >> tmpy >> tmpz;
+        x = static_cast<int>((tmpx + 1)/2.0f*w);
+        y = static_cast<int>((tmpy + 1)/2.0f*h);
+        v = new Vertex({{x, y}, NULL});
+        _vertices.push_back(v);
+    }
+    * Pravljenje polja i poluivica. *
+    int firstIdx;
+    for(int i=0; i<fieldNum; i++)
+    {
 }
+
+DCEL::DCEL(const std::vector<QPoint> &tacke)
+    :_vertices{}, _edges{}, _fields{}
+{
+    loadData(tacke);
+}
+
+DCEL::DCEL()
+    :_vertices{}, _edges{}, _fields{}
+{}
+
+std::vector<Vertex *> DCEL::vertices() const
+{
+    return _vertices;
+}
+
+std::vector<Vertex*> &DCEL::v()
+{
+    return _vertices;
+}
+
+const std::vector<Vertex*> *DCEL::cv() const
+{
+    return &_vertices;
+}
+
+std::vector<HalfEdge *> DCEL::edges() const
+{
+    return _edges;
+}
+
+std::vector<HalfEdge *> &DCEL::e()
+{
+    return _edges;
+}
+
+const std::vector<HalfEdge *> *DCEL::ce() const
+{
+    return &_edges;
+}
+
+std::vector<Field *> DCEL::fields() const
+{
+    return _fields;
+}
+
+std::vector<Field*> &DCEL::f()
+{
+    return _fields;
+}
+
+const std::vector<Field*> *DCEL::cf() const
+{
+    return &_fields;
+}
+
+void DCEL::setFields(std::vector<Field *> &fileds)
+{
+    _fields = fileds;
+}
+
+void DCEL::loadData(const std::vector<QPoint> &tacke)
+{
+    ...............
+}
+
+void DCEL::insertEdge(HalfEdge *e)
+{
+    _edges.push_back(e);
+}
+
+void DCEL::insertFiled(Field *f)
+{
+    _fields.push_back(f);
+}
+
+HalfEdge *DCEL::findEdge(Vertex *start, Vertex *end)
+{
+    for(HalfEdge* e : _edges)
+    {
+        if (e->origin() == start && e->twin()->origin() == end)
+            return e;
+    }
+
+    return nullptr;
+}
+
+***********************************************************************
+*                             VERTEX                                  *
+***********************************************************************
+
+Vertex::Vertex()
+    :_coordinates{}, _incidentEdge{nullptr}, _obradjen{false}
+{}
+
+Vertex::Vertex(QPoint coordinates, HalfEdge *incidentEdge)
+    :_coordinates{coordinates}, _incidentEdge{incidentEdge}
+{}
+
+QPoint Vertex::coordinates() const
+{
+    return _coordinates;
+}
+
+void Vertex::setCoordinates(const QPoint &coordinates)
+{
+    _coordinates = coordinates;
+}
+
+HalfEdge *Vertex::incidentEdge() const
+{
+    return _incidentEdge;
+}
+
+void Vertex::setIncidentEdge(HalfEdge *incidentEdge)
+{
+    _incidentEdge = incidentEdge;
+}
+
+***********************************************************************
+*                           HALFEDGE                                 *
+***********************************************************************
+
+HalfEdge::HalfEdge()
+    :_origin{nullptr}, _twin{nullptr}, _next{nullptr}, _prev{nullptr}, _incidentFace{nullptr}, _helper{nullptr}
+{}
+
+HalfEdge::HalfEdge(Vertex *origin, HalfEdge *twin, HalfEdge *next, HalfEdge *prev, Field *incidentFace)
+    :_origin(origin), _twin(twin), _next(next), _prev(prev), _incidentFace(incidentFace)
+{}
+
+Vertex *HalfEdge::origin() const
+{
+    return _origin;
+}
+
+void HalfEdge::setOrigin(Vertex *origin)
+{
+    _origin = origin;
+}
+
+HalfEdge *HalfEdge::twin() const
+{
+    return _twin;
+}
+
+void HalfEdge::setTwin(HalfEdge *twin)
+{
+    _twin = twin;
+}
+
+HalfEdge *HalfEdge::next() const
+{
+    return _next;
+}
+
+void HalfEdge::setNext(HalfEdge *next)
+{
+    _next = next;
+}
+
+HalfEdge *HalfEdge::prev() const
+{
+    return _prev;
+}
+
+void HalfEdge::setPrev(HalfEdge *prev)
+{
+    _prev = prev;
+}
+
+Field *HalfEdge::incidentFace() const
+{
+    return _incidentFace;
+}
+
+void HalfEdge::setIncidentFace(Field *incidentFace)
+{
+    _incidentFace = incidentFace;
+}
+
+***********************************************************************
+*                             FIELD                                   *
+***********************************************************************
+
+Field::Field()
+    :_outerComponent{nullptr}, _inerComponents{}
+{}
+
+Field::Field(HalfEdge *outerComponent, std::vector<HalfEdge *> inerComponent)
+    :_outerComponent{outerComponent}, _inerComponents{inerComponent}
+{}
+
+HalfEdge *Field::outerComponent() const
+{
+    return _outerComponent;
+}
+
+void Field::setOuterComponent(HalfEdge *outerComponent)
+{
+    _outerComponent = outerComponent;
+}
+
+std::vector<HalfEdge *> Field::inerComponents() const
+{
+    return _inerComponents;
+}
+
+HalfEdge *Field::innerComponent() const
+{
+    if(inerComponents().empty())
+    {
+        return nullptr;
+    }
+
+    return _inerComponents[0];
+}
+
+void Field::setInerComponents(std::vector<HalfEdge *> inerComponents)
+{
+    _inerComponents = inerComponents;
+}
+
+void Field::setInnerComponent(HalfEdge* innerComponent)
+{
+    _inerComponents.push_back(innerComponent);
+}
+
+
+
+*/
--- Geometrijski_Algoritmi/algoritmi_sa_vezbi/ga06_dcel.h
+++ Geometrijski_Algoritmi/algoritmi_sa_vezbi/ga06_dcel.h
@@ -1,11 +1,80 @@
 #ifndef DCEL_H
 #define DCEL_H
 
+#include <QPoint>
+#include <QWidget>
 
+class Vertex;
+class HalfEdge;
+class Field;
+
+///
+/// \brief The DCEL class
+/// Dvostruko povezna lista (Doubly-Connected Edge List)
+/// Vise procitati u knjizi Computational Geometry
+///
 class DCEL
 {
 public:
+    DCEL(std::string imeDatoteke, int h, int w);
+
+    ///
+    /// \brief DCEL
+    /// Poligon zadatak tackama u pozitivnom matematickom smeru
+    /// bez rupa
+    ///
+    DCEL(const std::vector<QPoint> &tacke);
+
     DCEL();
+    //....
+private:
+    //...
+};
+
+///
+/// \brief The Vertex class
+/// Teme poligona
+/// Sadrzi koordinate
+/// i jednu incident (proizvoljnu) HalfEdge
+class Vertex{
+public:
+
+
+private:
+
+};
+
+
+///
+/// \brief The HalfEdge class
+/// Ovo je zapravo jedna ivica poligona, ali ivica moze imati dva smera, pa je
+/// sa HalfEdge oznacen jedan smer, a ona na koju pokazuje (twin) je ista ivica, ali drugi smer
+/// IncidentFace je face (strana) koja je sa leve strane posmatrane HalfEdge
+/// Next i Prev su prethodna i sledeca HalfEdge
+/// Origin je Vrtex koji je pocetak HalfEdge
+/// Vise o ovoj strukturi procitati u Computational Geoemtry
+///
+class HalfEdge{
+public:
+
+
+private:
+};
+
+///
+/// \brief The Field class
+/// Sadrzi dva niza HalfEdge
+/// Prvi je OuterComponent i pokazuje na one HalfEdge koje su na granici (boundary) posmatrane stranice
+/// Za one stranice koje nemaju granicu, tj. koje su nepovezane taj niz je nil (tj. prazan)
+/// Drugi niz je InnerComponent i pokazuje na one HalfEdge koje cine "rupu" u okviru posmatrane stranice
+/// Moze biti prazan
+/// Vise o ovoj strukturi procitati u Computational Geoemtry
+///
+class Field{
+public:
+
+
+private:
 };
 
 #endif // DCEL_H
