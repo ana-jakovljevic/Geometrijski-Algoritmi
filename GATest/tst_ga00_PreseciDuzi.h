c0096d1de836f740ada2ed16b3671d43a8e22df7
lcikaric <cikariclidija95@gmail.com>
2020-12-23 16:00:29 +0100

    test: PreseciDuzi

---
+++ GATest/tst_ga00_PreseciDuzi.h
@@ -0,0 +1,45 @@
+#ifndef TST_GA00_PRESECIDUZI_H
+#define TST_GA00_PRESECIDUZI_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "./ga05_preseciduzi.h"
+
+using namespace testing;
+
+static const auto duzi = "../Geometrijski_Algoritmi/input_files/ga00_PreseciDuzi/input1.txt";
+
+TEST(ga05_preseciduzi, test1)
+{
+    PreseciDuzi pd(nullptr, 0, false, "", 5);
+    pd.pokreniAlgoritam();
+    pd.pokreniNaivniAlgoritam();
+    EXPECT_LE(pd.getPreseci().size(), pd.getNaivniPreseci().size());
+}
+
+TEST(ga05_preseciduzi, test2)
+{
+    PreseciDuzi pd(nullptr, 0, false, "", 150);
+    pd.pokreniAlgoritam();
+    pd.pokreniNaivniAlgoritam();
+    EXPECT_LE(pd.getPreseci().size(), pd.getNaivniPreseci().size());
+}
+
+TEST(ga05_preseciduzi, test3)
+{
+    PreseciDuzi pd(nullptr, 0, false, "", 1000);
+    pd.pokreniAlgoritam();
+    pd.pokreniNaivniAlgoritam();
+    EXPECT_LE(pd.getPreseci().size(), pd.getNaivniPreseci().size());
+}
+
+TEST(ga05_preseciduzi, test4)
+{
+    PreseciDuzi pd(nullptr, 0, false, duzi, 0);
+    pd.pokreniAlgoritam();
+    pd.pokreniNaivniAlgoritam();
+    EXPECT_LE(pd.getPreseci().size(), pd.getNaivniPreseci().size());
+}
+
+#endif // TST_GA00_PRESECIDUZI_H
