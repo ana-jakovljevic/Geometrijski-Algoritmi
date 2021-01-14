a367473fe23c6f1b85e6c9ca8727aa0a842e76c0
Dara Milojkovic <milojkovic.dara@gmail.com>
2021-01-14 18:10:21 +0100

    testovi

--- GATest/tst_ga18_ShortestPath.h
+++ GATest/tst_ga18_ShortestPath.h
@@ -9,9 +9,46 @@
 using namespace testing;
 
 static constexpr auto poligoni = "../Geometrijski_Algoritmi/input_files/ga18_ShortestPath/ulaz.txt";
+static constexpr auto poligoni1 = "../Geometrijski_Algoritmi/input_files/ga18_ShortestPath/ulaz1.txt";
 
-TEST(ga18_shortestPath, jednakost_najkracih_puteva_duzina){
+TEST(ga18_shortestPathTests, jednakost_najkracih_puteva_duzina){
     ShortestPath sp(nullptr, 0, false, "", 10);
+
+    sp.pokreniAlgoritam();
+    auto dobijenoAlgoritmom = sp.getNajkraciPut().size();
+    auto duzinaDobijenogAlgoritma = sp.getDuzinaNajkracegPuta();
+    sp.pokreniNaivniAlgoritam();
+    auto dobijenoNaivnim = sp.getNajkraciPut().size();
+    auto duzinaNaivnogAlgoritma = sp.getDuzinaNajkracegPuta();
+    ASSERT_EQ(dobijenoAlgoritmom, dobijenoNaivnim);
+    ASSERT_EQ(duzinaDobijenogAlgoritma, duzinaNaivnogAlgoritma);
+}
+
+TEST(ga18_shortestPathTests, jednakost_najkracih_puteva_vrednost){
+    ShortestPath sp(nullptr, 0, false, "", 20);
+
+    sp.pokreniAlgoritam();
+    auto dobijenoAlgoritmom = sp.getNajkraciPut().size();
+    auto duzinaDobijenogAlgoritma = sp.getDuzinaNajkracegPuta();
+    sp.pokreniNaivniAlgoritam();
+    auto dobijenoNaivnim = sp.getNajkraciPut().size();
+    auto duzinaNaivnogAlgoritma = sp.getDuzinaNajkracegPuta();
+    ASSERT_EQ(dobijenoAlgoritmom, dobijenoNaivnim);
+    ASSERT_EQ(duzinaDobijenogAlgoritma, duzinaNaivnogAlgoritma);
+}
+
+TEST(ga18_shortestPathTests, jednakost_najkracih_puteva_iz_datoteke_vrednost){
+    ShortestPath sp(nullptr, 0, false, poligoni, 0);
+
+    sp.pokreniAlgoritam();
+    auto dobijenoAlgoritmom = sp.getDuzinaNajkracegPuta();
+    sp.pokreniNaivniAlgoritam();
+    auto dobijenoNaivnim = sp.getDuzinaNajkracegPuta();
+    ASSERT_EQ(dobijenoAlgoritmom, dobijenoNaivnim);
+}
+
+TEST(ga18_shortestPathTests, jednakost_najkracih_puteva_iz_datoteke_duzina){
+    ShortestPath sp(nullptr, 0, false, poligoni, 0);
 
     sp.pokreniAlgoritam();
     auto dobijenoAlgoritmom = sp.getNajkraciPut().size();
@@ -20,13 +57,23 @@ TEST(ga18_shortestPath, jednakost_najkracih_puteva_duzina){
     ASSERT_EQ(dobijenoAlgoritmom, dobijenoNaivnim);
 }
 
-TEST(ga18_shortestPath, jednakost_najkracih_puteva_vrednost){
-    ShortestPath sp(nullptr, 0, false, "", 10);
+TEST(ga18_shortestPathTests, jednakost_najkracih_puteva_iz_datoteke1_vrednost){
+    ShortestPath sp(nullptr, 0, false, poligoni1, 0);
 
     sp.pokreniAlgoritam();
     auto dobijenoAlgoritmom = sp.getDuzinaNajkracegPuta();
     sp.pokreniNaivniAlgoritam();
     auto dobijenoNaivnim = sp.getDuzinaNajkracegPuta();
+    ASSERT_EQ(dobijenoAlgoritmom, dobijenoNaivnim);
+}
+
+TEST(ga18_shortestPathTests, jednakost_najkracih_puteva_iz_datoteke1_duzina){
+    ShortestPath sp(nullptr, 0, false, poligoni1, 0);
+
+    sp.pokreniAlgoritam();
+    auto dobijenoAlgoritmom = sp.getNajkraciPut().size();
+    sp.pokreniNaivniAlgoritam();
+    auto dobijenoNaivnim = sp.getNajkraciPut().size();
     ASSERT_EQ(dobijenoAlgoritmom, dobijenoNaivnim);
 }
 #endif // TST_GA18_SHORTESTPATH_H
